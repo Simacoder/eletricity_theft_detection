dcbe5d4f766223d896b83ff5303cd5d33f5ef9ff
Bongani <bonganibaloyi94@gmail.com>
2025-03-18 18:12:33 +0200

    Update

--- Web app/backend/app/core/config.py
+++ Web app/backend/app/core/config.py
@@ -1,27 +1,26 @@
-import os
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    DATABASE_NAME: str = os.getenv("DATABASE_NAME")
-    DATABASE_USER: str = os.getenv("DATABASE_USER")
-    DATABASE_PASSWORD: str = os.getenv("DATABASE_PASSWORD")
-    DATABASE_HOST: str = os.getenv("DATABASE_HOST")
-    DATABASE_PORT: int = os.getenv("DATABASE_PORT")
+    DATABASE_URL: str
+    DATABASE_NAME: str
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str
+    DATABASE_PORT: int
 
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
-    ALGORITHM: str = os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    FRAUD_DETECTION_MODEL_PATH: str = os.path.join(os.getcwd(), "fraud_detection_model.pkl")
+    FRAUD_DETECTION_MODEL_PATH: str
+    
+    log_level: str
 
     class Config:
         env_file = ".env"
+        env_file_encoding = 'utf-8'
 
 settings = Settings()
